baf48df9082b31fdc94d3268faa1c24ed1685f73
vaishnavi shelar <shelarvaishnavi95@gmail.com>
2026-05-30 18:05:39 +0530

    Add files via upload

---
+++ C Programming/Competitive programming/Assignment 6/Q3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+    int iCnt=0;
+    int iFact = 1;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    for(iCnt = 1;iCnt<=iNo;iCnt++)
+    {
+        iFact=iFact*iCnt;
+    }
+    return iFact;
+}
+
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+    iRet=Factorial(iValue);
+    printf("Factorial of Number is %d\n",iRet);
+    return 0;
+}
