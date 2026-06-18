325e3c2ac06b662f2a02d8dbb8f0d06dd2fdaa7b
vaishnavi shelar <shelarvaishnavi95@gmail.com>
2026-06-18 18:42:05 +0530

    Add files via upload

---
+++ C Programming/Competitive programming/Assignment 26/Q2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+    int i = 0,j = 0;
+    if(iRow != iCol)
+        {
+            printf("Invalid parameter");
+            printf("Number of rows and columns should be same");
+            return;
+        }
+    for(i = 1;i <= iRow;i++)
+    {
+        for(j = 1;j <= iCol;j++)
+        {
+            if(i==j || j<=i)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d",&iValue1,&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
